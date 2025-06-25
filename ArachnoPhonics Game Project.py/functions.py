62e36cfdf4e009b31f0b9dd4fa1b6832ebe09a3e
Alberto.G <145074162+Gspidie@users.noreply.github.com>
2025-06-25 22:07:15 +0000

    Updating repo with functions.py, main.py, spiderDraw.py

---
+++ ArachnoPhonics Game Project.py/functions.py
@@ -0,0 +1,105 @@
+import random
+import os
+import time
+
+# Prompts user for letter guess. Checks through the secret word to see if it contains the letter guessed by the user. Returns the number of wrong guesses
+#Takes in the correct letter list, incorrect letter list, secret word and the number of tries as parameters.
+
+def check_word(guess, word, correct, incorrect, tries):
+
+  if not is_guess_valid(guess):
+    print("Please input only one letter \n")
+    return correct, incorrect, tries
+  
+  if already_guessed(guess, incorrect, correct):
+    print("You already guessed that.")
+    return correct, incorrect, tries
+
+  if is_guess_correct(guess, word):
+    correct.append(guess)
+    print(f"{guess} is in the hidden word!")
+    time.sleep(2)
+    os.system('clear')
+  else:
+    incorrect.append(guess)
+    tries += 1
+    print(f"{guess} is not in the word!")
+    time.sleep(2)
+    os.system('clear')
+
+  return correct, incorrect, tries
+
+  
+'''
+def check_word():
+  guess = input("Enter a valid guess: ")
+
+  if guess.isaplha() and len(guess) == 1:
+    return guess
+  else:
+    check_word()
+'''
+    
+def is_guess_correct(guess, word):
+  return guess.lower() in word
+
+def is_guess_valid(guess):
+  return (guess.isalpha() and len(guess) == 1)
+
+
+# Returns the word to the console containing "_" for any letter not guessed by the user.
+#Takes in the correct word and the list of correct guesses as parameters
+def print_word(correctWord, correctGuesses):
+  word = " "
+
+  for i, letter in enumerate(correctWord):
+    if letter not in correctGuesses:
+      word += "_"
+    else: 
+      word += letter
+  
+  return word
+
+
+
+# Prints spider from the spider drawing functions in the spiderDraw.py file. Takes the number of wrong guesses and the list of spider drawing functions as parameters.
+
+def print_spider(tries,spiderList):
+  spiderList[tries]()
+
+
+#Opens the word list text file, stores the contents into a list, chooses a random word from the list, finds the length of that word and prints a string of blanks for each letter in the word. Returns the word.
+def generate_word():
+  wordList = open('/workspaces/SDE2-python-env-starter-GSP/ArachnoPhonics Game Project.py/words.txt').read().split()
+  word = random.choice(wordList)
+  print('Word = ' + '_'*len(word))
+  return word
+
+# Returns True if user's guess is already in incorrect or correct list
+def already_guessed(guess, incorrectGuesses, correctGuesses):
+  return guess in incorrectGuesses or guess in correctGuesses
+
+# Prints wrong guesses
+def print_wrong_guesses(incorrectGuesses):
+  print(f'Incorrect Guesses = {incorrectGuesses}')
+
+#Put the introduction code/input player name into here 
+def introduction():
+  print("Welcome to ArachnoPhonics! \n")
+  userName = input("What is your name? \n")
+  print(f'{userName}, your goal is to guess the word, letter by letter. \n')
+  print("Do not let the spider get you! \n")
+
+def won_game(word, correctGuesses):
+  for letter in word:
+    if letter not in correctGuesses:
+      return False
+
+  return True
+
+
+def lost_game(tries):
+  return tries == 6
+      
+
+  
---
+++ ArachnoPhonics Game Project.py/main.py
@@ -0,0 +1,56 @@
+#access libraries and py files 
+import time
+import os
+import spiderDraw as sd
+import functions as md
+
+#Make a list of the spider drawings
+spiderList = [sd.spider_0, sd.spider_1, sd.spider_2, sd.spider_3, sd.spider_4, sd.spider_5, sd.spider_6]
+
+
+
+def play_game():
+  #generate a random word from word list
+  word = md.generate_word()  
+
+  #Initialize variables and setup 
+  #Need to keep track of correct letters, incorrect letters and tries
+  correct = []  #List of correct letters guessed
+  incorrect = []  #List of incorrect letters guessed
+  tries = 0   #Number of incorrect guesses
+  game = True 
+
+  #Print intro statements (welcome to game, etc)
+  md.introduction()
+
+
+  #Game Loop
+  while not md.won_game(word, correct) and not md.lost_game(tries): 
+    #time.sleep(1)
+    if len(correct) > 0 or len(incorrect) > 0:
+      print(f"Word = {md.print_word(word, correct)}")
+    md.print_spider(tries,spiderList)
+    md.print_wrong_guesses(incorrect)
+    guess = input('Guess a letter: ')
+    correct, incorrect, tries = md.check_word(guess, word, correct, incorrect, tries)
+
+  if md.lost_game(tries):
+    print("You lost!")
+
+  if md.won_game(word, correct):
+    print("You won!")
+
+  anwser = input("Do you wanna play again (y/n)? ")
+
+  play_again = anwser == "y"
+  
+  if play_again: 
+    print("Nice to have you back! \n")
+  else:
+    print("Have a good day. Thank you for playing!")
+
+  return play_again
+
+playing_game = True
+while playing_game:
+  playing_game = play_game()
